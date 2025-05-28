8cfb480c1d1d076bd2e742cbd00c4ad97d08c4d5
Kovács Ferenc <83652379+Ferref@users.noreply.github.com>
2025-05-28 21:58:14 +0200

    Smoothing

--- laravel/resources/js/game/Parts/Player.js
+++ laravel/resources/js/game/Parts/Player.js
@@ -8,7 +8,8 @@ export default class Player {
         // Animations
         this.images = {
             idle: 'textures/lilheroes/1 Pink_Monster/Pink_Monster_Idle_4.png',
-            moveUp: 'textures/lilheroes/1 Pink_Monster/Pink_Monster_Jump_8.png',
+            moveUp: 'textures/lilheroes/1 Pink_Monster/Pink_Monster_Climb_4.png',
+            moveDown: 'textures/lilheroes/1 Pink_Monster/Pink_Monster_Walk_6.png',
             run: 'textures/lilheroes/1 Pink_Monster/Pink_Monster_Run_6.png',
         }
     }
--- laravel/resources/js/main.js
+++ laravel/resources/js/main.js
@@ -11,17 +11,19 @@ $(document).ready(() => {
 
     const frameSize = 32;
     const totalFrames = 4;
-    const frameDelay = 100;
+    const frameDelay = 50;
     let currentFrame = 0;
 
     const images = {
         idle: new Image(),
         moveUp: new Image(),
-        run: new Image()
+        moveDown: new Image(),
+        run: new Image(),
     };
 
     images.idle.src = player.images.idle;
     images.moveUp.src = player.images.moveUp;
+    images.moveDown.src = player.images.moveDown;
     images.run.src = player.images.run;
 
     let currentImage = images.idle;
@@ -39,10 +41,11 @@ $(document).ready(() => {
                 player.moveUp();
                 currentImage = images.moveUp;
                 flipHorizontal = false;
+                idleSlow();
                 break;
             case 's':
                 player.moveDown();
-                currentImage = images.moveUp;
+                currentImage = images.moveDown;
                 flipHorizontal = false;
                 break;
             case 'd':
@@ -65,38 +68,51 @@ $(document).ready(() => {
     };
 
     const startAnimation = () => {
-        setInterval(() => {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
+        let lastFrameTime = 0;
 
-            const posX = 100 + player.positionX;
-            const posY = 100 + player.positionY;
-            const scale = 1;
+        const animate = (timestamp) => {
+            if (!lastFrameTime) lastFrameTime = timestamp;
+            const delta = timestamp - lastFrameTime;
 
-            if (flipHorizontal) {
-                ctx.save();
-                ctx.translate(posX + frameSize, posY);
-                ctx.scale(-1, scale);
-                ctx.drawImage(
-                    currentImage,
-                    currentFrame * frameSize, 0,
-                    frameSize, frameSize,
-                    0, 0,
-                    frameSize, frameSize
-                );
-                ctx.restore();
-            } else {
-                ctx.drawImage(
-                    currentImage,
-                    currentFrame * frameSize, 0,
-                    frameSize, frameSize,
-                    posX, posY,
-                    frameSize, frameSize
-                );
+            if (delta > frameDelay) {
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+                const posX = 100 + player.positionX;
+                const posY = 100 + player.positionY;
+                const scale = 1;
+
+                if (flipHorizontal) {
+                    ctx.save();
+                    ctx.translate(posX + frameSize, posY);
+                    ctx.scale(-1, scale);
+                    ctx.drawImage(
+                        currentImage,
+                        currentFrame * frameSize, 0,
+                        frameSize, frameSize,
+                        0, 0,
+                        frameSize, frameSize
+                    );
+                    ctx.restore();
+                } else {
+                    ctx.drawImage(
+                        currentImage,
+                        currentFrame * frameSize, 0,
+                        frameSize, frameSize,
+                        posX, posY,
+                        frameSize, frameSize
+                    );
+                }
+
+                currentFrame = (currentFrame + 1) % totalFrames;
+                lastFrameTime += frameDelay;
             }
 
-            currentFrame = (currentFrame + 1) % totalFrames;
-        }, frameDelay);
+            requestAnimationFrame(animate);
+        };
+
+        requestAnimationFrame(animate);
     };
+
 
     let imagesLoaded = 0;
 
@@ -111,4 +127,6 @@ $(document).ready(() => {
     images.idle.onload = onImageLoad;
     images.moveUp.onload = onImageLoad;
     images.run.onload = onImageLoad;
+    images.moveDown.onload = onImageLoad;
+    
 });
